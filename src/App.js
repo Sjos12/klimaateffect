f056e070228271efdd2535b218f1318872535269
sjos12 <denzelstellingwerf@outlook.com>
2021-11-07 11:18:31 +0100

    Added sass to website.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Landingpage from './Landingpage';
+import './sass/main.scss';
 export class App extends React.Component {
     render() {
         return (
-            
             <Landingpage />
         )
     }
