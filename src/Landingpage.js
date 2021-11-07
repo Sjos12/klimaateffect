8459958bcb1f2aebdb78c3c1d8f659533028efbb
sjos12 <denzelstellingwerf@outlook.com>
2021-11-07 11:03:11 +0100

    Added landing page component

---
+++ src/Landingpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Landingpage extends React.Component {
+
+    render() { 
+        return ( 
+            <div class=""> 
+                <h1>Klimaat effect</h1>
+                
+            </div>
+         );
+    }
+}
+ 
+export default Landingpage;
